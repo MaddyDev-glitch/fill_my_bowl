5bea6ba7e9b43301ad23df118c568a2fb975b25e
SriMadhaven <sri.madhaven@gmail.com>
2020-11-22 02:26:41 +0530

    Login done

---
+++ lib/LoginPage.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+final fbLogin = FacebookLogin();
+bool isLoggedIn = false;
+var det = "";
+
+Future<String> signInWithGoogle() async {
+  await Firebase.initializeApp();
+
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+  await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final UserCredential authResult =
+  await _auth.signInWithCredential(credential);
+  final User user = authResult.user;
+
+  if (user != null) {
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final User currentUser = _auth.currentUser;
+    assert(user.uid == currentUser.uid);
+
+    print('signInWithGoogle succeeded: ${user}');
+    det = user.displayName;
+    return '${user.displayName}';
+  }
+
+  return null;
+}
+
+void signOutGoogle() async {
+  await googleSignIn.signOut();
+
+  print("User Signed Out");
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  //====================================================================
+  void initiateFacebookLogin() async {
+    var facebookLogin = FacebookLogin();
+    var facebookLoginResult =
+    // await facebookLogin.logInWithReadPermissions(['name']);
+    await facebookLogin.logIn(["email"]);
+    print(facebookLoginResult.errorMessage);
+    print(facebookLoginResult.accessToken);
+    switch (facebookLoginResult.status) {
+      case FacebookLoginStatus.error:
+        print("Error");
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print("CancelledByUser");
+        onLoginStatusChanged(false);
+        break;
+      case FacebookLoginStatus.loggedIn:
+        {
+          print("LoggedIn");
+          onLoginStatusChanged(true);
+          // Navigator.of(context).push(
+          //   MaterialPageRoute(
+          //     builder: (context) {
+          //       return Testing("");
+          //     },
+          //   ),
+          // );
+          break;
+        }
+    }
+  }
+
+  void onLoginStatusChanged(bool isLoggedIn) {
+    setState(() {
+      isLoggedIn = isLoggedIn;
+    });
+  }
+
+  //=====================================================================
+  var loggedIn = false;
+  var firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            // color: Colors.white,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // FlutterLogo(size: 150),
+                  Image(image: AssetImage("images/logo.png"), height: 210.0),
+                  Text(
+                    'Fill My Bowl',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Be a part of the process',
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(height: 30),
+                  _googleSignInButton(),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Container(
+                child: Center(
+                  child: loggedIn
+                      ? Text("Logged In! :)",
+                      style: TextStyle(color: Colors.white, fontSize: 40))
+                      : Stack(
+                    children: <Widget>[
+                      Container(
+                        child: Center(
+                          child: _facebookLoginButton(),
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _googleSignInButton() {
+    return OutlineButton(
+      splashColor: Colors.blue,
+      onPressed: () async {
+        signInWithGoogle().then((result) {
+          if (result != null) {
+            // Navigator.of(context).push(
+            //   MaterialPageRoute(
+            //     builder: (context) {
+            //       return Testing(det);
+            //     },
+            //   ),
+            // );
+          }
+        });
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.blue),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("images/google_logo.png"), height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  OutlineButton _facebookLoginButton() {
+    return OutlineButton(
+      splashColor: Colors.blue,
+      onPressed: () {
+        initiateFacebookLogin();
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.blue),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("images/fbiconbig.png"), height: 40.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Facebook',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
